a8118631a4267864df60164f6f719c586a482518
Lukasz Kosminski <48951121+lukaszkosminski@users.noreply.github.com>
2023-12-29 11:53:08 +0100

    Add localhost configuration in Docker Compose

---
+++ src/test/java/com/todolist/model/UserTest.java
@@ -0,0 +1,15 @@
+package com.todolist.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    @Test
+    public void testIfUderCreatedForLegaLArgument(){
+        User user = new User();
+//        user.setRole("USER");
+//        user.setEmail("wp@wp.pl");
+    }
+
+}
